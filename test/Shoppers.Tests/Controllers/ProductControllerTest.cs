b07fa98766b3ca225cf0e058d5dd57924638fc3c
Afzal Qureshi <afzal.qureshi@outlook.com>
2016-06-05 13:07:40 +0530

    Completed Unit Tests

---
+++ test/Shoppers.Tests/Controllers/ProductControllerTest.cs
@@ -0,0 +1,42 @@
+﻿using Moq;
+using ProductCatalogService;
+using ProductCatalogServiceHost.Controllers;
+using Xunit;
+using System;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+using System.Threading;
+using Microsoft.AspNetCore.Http;
+using SharedServices.Model;
+using DomainModel.ProductCatalog;
+
+namespace Shoppers.Tests.Controllers
+{
+    public class ProductControllerTest
+    {
+        private  ProductController subjectunderTest;
+        private readonly Mock<IProductCatalogService> mockProductCatalogService;
+        public ProductControllerTest()
+        {
+            mockProductCatalogService = new Mock<IProductCatalogService>();
+            subjectunderTest = new ProductController(mockProductCatalogService.Object);
+           
+        }
+        [Fact]
+        public void Api_Should_Return_Bad_Request_When_Parameter_Is_Empty()
+        {
+            var result = Assert.IsType<BadRequestObjectResult>(subjectunderTest.Get(Guid.Empty).Result);
+            Assert.Equal(400, result.StatusCode);
+        }
+
+        [Fact]
+        public void Api_Should_Return_Internal_Server_Error_When_Promise_Has_Exception()
+        {
+            var mockPromise = new Promise<Product> { Exception = new Exception("Some Fake Error"), Result = null };
+            mockProductCatalogService.Setup(s => s.GetProductById(It.IsAny<Guid>()).Result).Returns(mockPromise);
+            subjectunderTest = new ProductController(mockProductCatalogService.Object);
+            var result = Assert.IsType<StatusCodeResult>(subjectunderTest.Get(Guid.NewGuid()).Result);
+            Assert.Equal(500, result.StatusCode);
+        } 
+    }
+}
